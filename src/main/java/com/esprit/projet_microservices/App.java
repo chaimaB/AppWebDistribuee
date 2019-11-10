8541ee57b294f452fc7273cac7bac6a90657e206
Achref Douss <achraf.douss@esprit.tn>
2019-11-11 00:11:15 +0100

    fix project

---
+++ src/main/java/com/esprit/projet_microservices/App.java
@@ -0,0 +1,13 @@
+package com.esprit.projet_microservices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
