51ecf2135bbb02315a031d44d8e0c321878c4fee
chaimaB <chaima.braiek@esprit.tn>
2019-11-12 20:02:29 +0100

    Materials micro service and update proxy

---
+++ Materiels-microservice/src/test/java/com/materiels/MaterielsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.materiels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterielsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
