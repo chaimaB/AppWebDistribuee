54ddbc742fd0eaf5bd360c38affa654a4c474de5
chaimaB <chaima.braiek@esprit.tn>
2019-11-12 15:09:48 +0100

    Human ressources microservice: crud user and holiday

--- Ressources-Humaines/src/main/java/com/ressourceshumaines/ressouceshumaines/services/HolidayService.java
+++ Ressources-Humaines/src/main/java/com/ressourceshumaines/ressouceshumaines/services/HolidayService.java
@@ -1,5 +1,6 @@
 package com.ressourceshumaines.ressouceshumaines.services;
 
+import com.netflix.discovery.converters.Auto;
 import com.ressourceshumaines.ressouceshumaines.entities.Holiday;
 import com.ressourceshumaines.ressouceshumaines.repositories.HolidayRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +12,26 @@ public class HolidayService {
     @Autowired
     private HolidayRepository holidayRepository;
 
-    public Holiday addHoliday(Holiday holiday) {
+    @Autowired
+    private UserService userService;
+
+    public Holiday addHoliday(Holiday holiday, String idUser) {
+        holiday.setUser(userService.getUserById(idUser));
         return holidayRepository.save(holiday);
     }
 
     public Holiday getOneHoliday( int id) {
         return holidayRepository.getOne(id);
+    }
+
+    public Holiday updateHoliday(int id, Holiday newHoliday) {
+        if (holidayRepository.findById(id).isPresent()) {
+            Holiday existingHoliday = holidayRepository.findById(id).get();
+            existingHoliday.setStartDate(newHoliday.getStartDate());
+            existingHoliday.setEndDate(newHoliday.getStartDate());
+
+            return holidayRepository.save(existingHoliday);
+        } else
+            return null;
     }
 }
